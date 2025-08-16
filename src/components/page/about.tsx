7d4d08b560487b7ada9c2baa56f0bd72c90333e7
Joan Marin Pages <yowoneballoo@gmail.com>
2025-08-16 10:57:46 +0000

    PONLWE EL TEXTO HOLA

--- src/components/page/about.tsx
+++ src/components/page/about.tsx
@@ -20,7 +20,7 @@ export function About() {
       <div className="container relative z-10 text-center text-white flex flex-col items-center">
         <div className="fade-in-up max-w-4xl">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline text-white">
-            Más que un Desarrollador, un <span className="animated-gradient-text">Socio Tecnológico</span>
+            Hola
           </h2>
           <p className="mx-auto text-white/80 md:text-xl/relaxed mt-6">
             Mi misión es entender tus objetivos de negocio para construir soluciones digitales que no solo funcionen, sino que generen un impacto real y medible. Me involucro en cada proyecto como si fuera mío, asegurando la excelencia técnica y una comunicación transparente en cada paso del camino.
