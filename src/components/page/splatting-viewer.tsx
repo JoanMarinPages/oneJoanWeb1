d7b325d3f9ac3427f51813d0385265409b9af1bc
Joan Marin Pages <yowoneballoo@gmail.com>
2025-08-16 00:26:23 +0000

    usa esta documetnaciacion para gaussian splatting https://r3f.docs.pmnd.

--- src/components/page/splatting-viewer.tsx
+++ src/components/page/splatting-viewer.tsx
@@ -1,27 +1,14 @@
-
 "use client"
 
 import React from 'react'
-import { Canvas, extend, Object3DNode } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { LumaSplatsThree, LumaSplatsSemantics } from "@lumaai/luma-web";
-
-// Make LumaSplatsThree available to R3F
-extend( { LumaSplats: LumaSplatsThree } );
-
-// For typeScript support:
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    lumaSplats: Object3DNode<LumaSplatsThree, typeof LumaSplatsThree>
-  }
-}
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Splat } from '@react-three/drei'
 
 export function SplattingViewer() {
   return (
     <Canvas dpr={[1, 2]} camera={{ fov: 45, position: [0, 0, 4] }} style={{ touchAction: 'none' }}>
-        <lumaSplats
-            semanticsMask={LumaSplatsSemantics.FOREGROUND}
-            source='https://lumalabs.ai/capture/822bac8d-70d6-404e-aaae-f89f46672c67'
+        <Splat
+            src='https://lumalabs.ai/capture/822bac8d-70d6-404e-aaae-f89f46672c67/splat'
             scale={1.5}
         />
       <OrbitControls autoRotate />
